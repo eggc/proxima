0e782b7f0165701ae18703eb91d2a83140012c82
eggc <no.eggchicken@gmail.com>
2024-07-31 02:17:54 +0900

    🐛 fix: Remove 's' from `/users` path

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resource :users, only: :show
+  resource :user, only: :show
   resources :productions, only: [:index, :new, :create, :edit, :update, :destroy]
 end
