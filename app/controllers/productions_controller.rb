4c23586c42460a6cd906debe75a1cb73341d747b
eggc <no.eggchicken@gmail.com>
2024-07-30 20:50:30 +0900

    ✨ feat: Add a create page for productions

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -2,4 +2,20 @@ class ProductionsController < ApplicationController
   def index
     @productions = Production.all
   end
+
+  def new
+    @production = Production.new
+  end
+
+  def create
+    @production = Production.new(production_params)
+    @production.save!
+    redirect_to(productions_path)
+  end
+
+  private
+
+  def production_params
+    params.require(:production).permit(:title, :media, :description)
+  end
 end
