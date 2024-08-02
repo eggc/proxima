acf854f3d07bea14372a099cfbfa4250c0300cf2
eggc <no.eggchicken@gmail.com>
2024-08-02 23:01:38 +0900

    ✨ feat: Add character pages

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,37 @@
+class CharactersController < ApplicationController
+  def index
+    @characters = Character.where(user: current_user)
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def edit
+    @character = Character.where(user: current_user).find(params[:id])
+  end
+
+  def create
+    @character = Character.new(user: current_user)
+    @character.update!(character_params)
+    redirect_to(characters_path)
+  end
+
+  def update
+    @character = Character.where(user: current_user).find(params[:id])
+    @character.update!(character_params)
+    redirect_to(characters_path)
+  end
+
+  def destroy
+    @character = Character.where(user: current_user).find(params[:id])
+    @character.destroy!
+    redirect_to(characters_path)
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :gender, :job, :age, :interests)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   root 'roots#show'
   resource :user, only: :show
   resources :projects, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :characters, only: [:index, :new, :create, :edit, :update, :destroy]
 end
