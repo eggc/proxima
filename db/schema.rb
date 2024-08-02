32492bed1b825fe0b07aa6a81af9890e5c0dd45a
eggc <no.eggchicken@gmail.com>
2024-08-02 22:50:17 +0900

    ✨ feat: Run `rails g model Character`

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_30_113216) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_02_134412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.string "gender"
+    t.string "job"
+    t.integer "age"
+    t.text "interests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_characters_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -37,5 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_30_113216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "characters", "users"
   add_foreign_key "projects", "users"
 end
