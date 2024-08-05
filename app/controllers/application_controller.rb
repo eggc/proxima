a7e0495e6d0dbfe91a0d55525b394bfbc63e1102
eggc <no.eggchicken@gmail.com>
2024-08-05 23:03:49 +0900

    ✨ feat: Add authorization to project pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :rescue_not_authorized_error
+
+  private
+
+  def rescue_not_authorized_error
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_back_or_to(root_path)
+  end
+
+  def verify_pundit_authorization
+    if action_name == 'index'
+      verify_policy_scoped
+    else
+      verify_authorized
+    end
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,35 +1,43 @@
 class ProjectsController < ApplicationController
+  after_action :verify_pundit_authorization
+
   def index
-    @projects = Project.public_or_owned_by(current_user).order(:display_order)
+    @projects = policy_scope(Project).order(:display_order)
   end
 
   def show
-    @project = Project.public_or_owned_by(current_user).find(params[:id])
+    @project = Project.find(params[:id])
+    authorize(@project)
   end
 
   def new
-    max_order = Project.where(user: current_user).maximum(:display_order)
-    @project = Project.new(display_order: max_order + 1)
+    max_order = Project.where(user: current_user).maximum(:display_order) || 0
+    @project = Project.new(user: current_user, display_order: max_order + 1)
+    authorize(@project)
   end
 
   def edit
-    @project = Project.where(user: current_user).find(params[:id])
+    @project = Project.find(params[:id])
+    authorize(@project)
   end
 
   def create
     @project = Project.new(user: current_user)
+    authorize(@project)
     @project.update!(project_params)
     redirect_to(projects_path)
   end
 
   def update
-    @project = Project.where(user: current_user).find(params[:id])
+    @project = Project.find(params[:id])
+    authorize(@project)
     @project.update!(project_params)
     redirect_to(projects_path)
   end
 
   def destroy
-    @project = Project.where(user: current_user).find(params[:id])
+    @project = Project.find(params[:id])
+    authorize(@project)
     @project.destroy!
     redirect_to(projects_path)
   end
