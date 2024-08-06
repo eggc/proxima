99fd47991ae9b829a85151e1974c37275eccda8a
eggc <no.eggchicken@gmail.com>
2024-08-06 15:46:32 +0900

    ✨ feat: Add emote_icon

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,13 @@ class Idea < ApplicationRecord
   belongs_to :user
 
   enum :emote, %w[blank hate love think star].to_h { [_1, _1] }, prefix: true
+
+  def emote_icon
+    {
+      'hate' => "\u{1F621}",
+      'love' => "\u{2764}\u{FE0F}",
+      'think' => "\u{2753}",
+      'star' => "\u{2B50}"
+    }[emote] || ''
+  end
 end
