b2934de59d4c8bf22774f310d2f663c41a249548
eggc <no.eggchicken@gmail.com>
2024-08-06 16:05:40 +0900

    ✨ feat: Remove the new idea page to make creation of idea faster

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,21 +5,16 @@ class IdeasController < ApplicationController
     @ideas = policy_scope(Idea).order(:display_order)
   end
 
-  def new
-    max_order = Idea.where(user: current_user).maximum(:display_order) || 0
-    @idea = Idea.new(user: current_user, display_order: max_order + 1)
-    authorize(@idea)
-  end
-
   def edit
     @idea = Idea.find(params[:id])
     authorize(@idea)
   end
 
   def create
-    @idea = Idea.new(user: current_user)
+    max_order = Idea.where(user: current_user).maximum(:display_order) || 0
+    @idea = Idea.new(user: current_user, display_order: max_order + 1)
     authorize(@idea)
-    @idea.update!(idea_params)
+    @idea.save!
     redirect_to(ideas_path)
   end
 
