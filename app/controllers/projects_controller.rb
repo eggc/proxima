fd875e6c88324dd7cbb072a3591b33f9b0aab09f
eggc <no.eggchicken@gmail.com>
2024-08-08 22:12:47 +0900

    ✨ feat: Add input form for idea project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,11 +14,14 @@ class ProjectsController < ApplicationController
   def new
     max_order = Project.where(user: current_user).maximum(:display_order) || 0
     @project = Project.new(user: current_user, display_order: max_order + 1)
+    @selectable_ideas = build_selectable_ideas
     authorize(@project)
   end
 
   def edit
     @project = Project.find(params[:id])
+    @selectable_ideas = build_selectable_ideas
+    @selected_idea_ids = @project.ideas.ids
     authorize(@project)
   end
 
@@ -47,5 +50,11 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:theme, :title, :media, :description, :display_order)
+  end
+
+  def build_selectable_ideas
+    Idea.where(user: current_user)
+      .order(:display_order)
+      .map { |idea| [idea.emote_icon + idea.body.truncate(30), idea.id] }
   end
 end
