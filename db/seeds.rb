6bbf9476dd1bbd9181a1a20082195678178e8b36
eggc <no.eggchicken@gmail.com>
2024-08-08 22:05:48 +0900

    ♻️ refactor: Remove seed_helper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 if Rails.env.development?
-  require_relative './seed_helper'
-
-  helper = SeedHelper.new
-  helper.import(User)
-  helper.import(Idea)
-  helper.import(Project)
-  helper.import(IdeaProject, method: :insert)
-  helper.import(Character)
-  helper.import(ProjectCharacter, method: :insert)
-  helper.import(ProjectPart)
+  puts 'You should run bin/rails db:fixtures:load instead of bin/rails db:seed to create test data'
 end
