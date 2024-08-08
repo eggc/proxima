da32a33b9e49c9a54ed5ad4f01f5c0a163bab0f3
eggc <no.eggchicken@gmail.com>
2024-08-08 22:17:23 +0900

    ✨ feat: Add idea table to the project detail page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ApplicationRecord
   has_many :project_characters
   has_many :characters, through: :project_characters
   has_many :project_parts
+  has_many :idea_projects
+  has_many :ideas, through: :idea_projects
 
   enum :media, %w[unspecified movie anime comic game novel].to_h { [_1, _1] }, prefix: true
   enum :visibility, %w[private public].to_h { [_1, _1] }, prefix: true
