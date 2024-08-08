c387ea8c8fea6f0c16d1c8ebf4cda941d8178f59
eggc <no.eggchicken@gmail.com>
2024-08-08 22:22:52 +0900

    ✨ feat: Add dependent destroy

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ApplicationRecord
   belongs_to :user
-  has_many :idea_projects
+  has_many :idea_projects, dependent: :destroy
   has_many :projects, through: :idea_projects
 
   enum :emote, %w[blank hate love think star].to_h { [_1, _1] }, prefix: true
