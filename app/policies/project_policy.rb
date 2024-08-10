541f04c38ca577721deb9da3d13fbcba9994cfe1
eggc <no.eggchicken@gmail.com>
2024-08-10 22:31:59 +0900

    ✨ feat: Do not show public projects to focus on private projects

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,11 +1,11 @@
 class ProjectPolicy < AbstractUserItemPolicy
   def index?
-    record.visibility_public? || record.user_id == user.id
+    record.user_id == user.id
   end
 
   class Scope < ApplicationPolicy::Scope
     def resolve
-      scope.public_or_owned_by(user)
+      scope.where(user:)
     end
   end
 end
