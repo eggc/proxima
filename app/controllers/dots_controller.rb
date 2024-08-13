d2142519beef5fd6bd9170590a1f3073a84346d6
eggc <no.eggchicken@gmail.com>
2024-08-13 22:50:48 +0900

    ✨ feat: Set a category for a new dot if dot is filtered by category

--- app/controllers/dots_controller.rb
+++ app/controllers/dots_controller.rb
@@ -12,8 +12,7 @@ class DotsController < ApplicationController
   end
 
   def create
-    max_order = Dot.where(user: current_user).maximum(:display_order) || 0
-    @dot = Dot.new(user: current_user, display_order: max_order + 1, workspace: current_workspace)
+    @dot = build_new_dot(current_user, current_workspace, params[:category])
     authorize(@dot)
     @dot.save!
     redirect_back_or_to(dots_path)
@@ -37,5 +36,12 @@ class DotsController < ApplicationController
 
   def dot_params
     params.require(:dot).permit(:content, :category, :display_order)
+  end
+
+  def build_new_dot(user, workspace, category)
+    max_order = Dot.where(user:).maximum(:display_order) || 0
+    @dot = Dot.new(user:, display_order: max_order + 1, workspace:)
+    @dot.category = category if category.present?
+    @dot
   end
 end
