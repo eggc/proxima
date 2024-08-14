fe438ba0c00c248c7b2c247a35211cc011ea25fc
eggc <no.eggchicken@gmail.com>
2024-08-14 22:15:05 +0900

    🔧 config: Install ralis-i18n

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,11 @@ module Proxima
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone='Asia/Tokyo'
+
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.default_locale = :ja
+
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
