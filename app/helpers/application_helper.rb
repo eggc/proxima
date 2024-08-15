044f4d4ceed33de93b2283c5ab483b3084f59cb1
eggc <no.eggchicken@gmail.com>
2024-08-15 21:49:25 +0900

    ✨ feat: Use small icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def turbo_destroy_link_to(label, path, confirm:)
+  def turbo_destroy_link_to(label, path, confirm:, class: 'btn btn-sm')
     link_to(
       label,
       path,
-      class: 'btn btn-sm',
+      class:,
       data: {
         turbo_method: :delete,
         turbo_confirm: confirm ? 'Are you sure you want to delete this item? This action cannot be undone.' : nil
