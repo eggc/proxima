c0a357bafd887d92eaae9df51091fa5fed82b0fc
eggc <no.eggchicken@gmail.com>
2024-08-15 21:49:39 +0900

    ✨ feat: Add id anchor to the done button

--- app/controllers/dot_tasks_controller.rb
+++ app/controllers/dot_tasks_controller.rb
@@ -2,6 +2,6 @@ class DotTasksController < ApplicationController
   def create
     @dot_task = DotTask.new(dot_id: params[:dot_id])
     @dot_task.save!
-    redirect_back_or_to(dots_path)
+    redirect_to(dots_path(anchor: params[:dot_id]), status: :see_other)
   end
 end
