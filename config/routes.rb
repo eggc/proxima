fea61e42617f28c4ce0ef02a661a9f66b7daec3b
eggc <no.eggchicken@gmail.com>
2024-08-17 02:03:40 +0900

    ♻️ refactor: Rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,22 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'roots#show'
 
-  resources :workspaces, only: [:new, :edit, :create, :update, :destroy]
+  resources :workspaces, only: %i[new edit create update destroy]
   resources :current_workspaces, only: :update
   resource :tree, only: :show
   resource :user, only: :show
 
-  resources :dots, only: [:index, :create, :edit, :update, :destroy]
+  resources :dots, only: %i[index create edit update destroy]
   resources :dot_tasks, only: [:create]
 
-  resources :houseworks, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :housework_logs, only: [:create, :destroy]
+  resources :houseworks, only: %i[index new create edit update destroy]
+  resources :housework_logs, only: %i[create destroy]
 
-  resources :projects, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :project_parts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :characters, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :projects, only: %i[index show new create edit update destroy]
+  resources :project_parts, only: %i[index show new create edit update destroy]
+  resources :characters, only: %i[index show new create edit update destroy]
 end
