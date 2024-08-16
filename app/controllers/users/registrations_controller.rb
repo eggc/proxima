e3e3ab7706783dd0bfa9712f66b9d9782c4966bc
eggc <no.eggchicken@gmail.com>
2024-08-16 23:40:27 +0900

    ✨ feat: Create default setting after a user is registered

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(user)
+    UserSetting.create!(
+      user:,
+      creative_tool_enabled: true,
+      housework_tool_enabled: false
+    )
+
     workspace = Workspace.create!(user:, name: 'default workspace', display_order: 1)
     cookies[:current_workspace_id] = workspace.id
 
--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -1,2 +1,3 @@
 class UserSetting < ApplicationRecord
+  belongs_to :user
 end
