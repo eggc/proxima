40229f5c7fde2676b8cb7fd5c89ded5c707a49e7
eggc <no.eggchicken@gmail.com>
2024-08-16 23:55:10 +0900

    ✨ feat: Add housework pages

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -7,12 +7,15 @@ class HeaderComponent < ViewComponent::Base
   MenuItem = Data.define(:name, :path)
 
   def menu_items
-    return [] if current_user.nil?
-
-    menu_items = []
-    menu_items << MenuItem.new('Tree', tree_path) if creative_tool_enabled?
-    menu_items << MenuItem.new('Dots', dots_path) if creative_tool_enabled?
-    menu_items << MenuItem.new('User', user_path)
+    if current_user
+      [
+        *creative_tool_menu_items,
+        *housework_tool_menu_items,
+        MenuItem.new('User', user_path)
+      ]
+    else
+      []
+    end
   end
 
   def header_context
@@ -23,7 +26,22 @@ class HeaderComponent < ViewComponent::Base
 
   private
 
-  def creative_tool_enabled?
-    current_user.user_setting.creative_tool_enabled
+  def creative_tool_menu_items
+    if current_user.user_setting.creative_tool_enabled
+      [
+        MenuItem.new('Tree', tree_path),
+        MenuItem.new('Dots', dots_path)
+      ]
+    else
+      []
+    end
+  end
+
+  def housework_tool_menu_items
+    if current_user.user_setting.housework_tool_enabled
+      [MenuItem.new('Housework', houseworks_path)]
+    else
+      []
+    end
   end
 end
---
+++ app/controllers/houseworks_controller.rb
@@ -0,0 +1,40 @@
+class HouseworksController < ApplicationController
+  after_action :verify_pundit_authorization
+
+  def index
+    @houseworks = policy_scope(Housework).order(display_order: :desc)
+  end
+
+  def edit
+    @housework = Housework.find(params[:id])
+    authorize(@housework)
+  end
+
+  def create
+    max_order = Housework.where(user: current_user).maximum(:display_order) || 0
+    @housework = Housework.new(user: current_user, display_order: max_order + 1)
+    authorize(@housework)
+    @housework.save!
+    redirect_back_or_to(houseworks_path)
+  end
+
+  def update
+    @housework = Housework.find(params[:id])
+    authorize(@housework)
+    @housework.update!(housework_params)
+    redirect_to(houseworks_path)
+  end
+
+  def destroy
+    @housework = Housework.find(params[:id])
+    authorize(@housework)
+    @housework.destroy!
+    redirect_to(houseworks_path)
+  end
+
+  private
+
+  def housework_params
+    params.require(:housework).permit(:content, :display_order)
+  end
+end
