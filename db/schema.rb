33b103b58d7d08ff8c9a27fbd287b75344f6956c
eggc <no.eggchicken@gmail.com>
2024-08-16 22:09:39 +0900

    ✨ feat: Add UserSetting model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_124142) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_16_130239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_124142) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
+  create_table "user_settings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.boolean "housework_tool_enabled", null: false
+    t.boolean "creative_tool_enabled", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_settings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -127,5 +136,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_124142) do
   add_foreign_key "project_parts", "projects"
   add_foreign_key "project_parts", "users"
   add_foreign_key "projects", "users"
+  add_foreign_key "user_settings", "users"
   add_foreign_key "workspaces", "users"
 end
