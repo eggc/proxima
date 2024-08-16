e9915757729fe667fe59c9cc94873d9c7ff9f421
eggc <no.eggchicken@gmail.com>
2024-08-17 00:42:01 +0900

    ✨ feat: Add buttom navigation

---
+++ app/components/bottom_navigation_component.rb
@@ -0,0 +1,18 @@
+class BottomNavigationComponent < ViewComponent::Base
+  delegate :current_user, to: :controller, private: true
+
+  def home?
+    controller.controller_name == 'houseworks' &&
+      controller.action_name == 'index'
+  end
+
+  def new_item?
+    controller.controller_name == 'houseworks' &&
+      controller.action_name == 'new'
+  end
+
+  def user?
+    controller.controller_name == 'users' &&
+      controller.action_name == 'show'
+  end
+end
