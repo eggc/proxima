9d67dcbd923909d9fb584403432d8832343d35cb
eggc <no.eggchicken@gmail.com>
2024-08-20 14:01:29 +0900

    ✨ feat: Add email confirmation

---
+++ db/migrate/20240820043617_add_confirmable_to_users.rb
@@ -0,0 +1,10 @@
+class AddConfirmableToUsers < ActiveRecord::Migration[7.1]
+  def change
+    change_table(:users) do |t|
+      t.string   :confirmation_token, index: { unique: true }
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+    end
+  end
+end
