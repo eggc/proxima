34438484a5e82598084fc36bf58f885eb3a6db73
eggc <no.eggchicken@gmail.com>
2024-08-20 15:44:21 +0900

    🔧 config: Add production host setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "proxima_production"
 
+  config.action_mailer.default_url_options = { host: ENV.fetch("PROXIMA_HOST") }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
